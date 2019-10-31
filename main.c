be93a9dbbcfe24eec0a72055a7390ddd915cc2b0
lexyaa <55127454+lexyaa@users.noreply.github.com>
2019-10-31 16:09:34 +0900

    09_06

--- main.c
+++ main.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+{
+	int i, j;
+	for(i=0;i<ROWS;i++)
+	{
+		for(j=0;j<COLS;j++)
+		 C[i][j]=A[i][j]+B[i][j];
+	}
+}
+
+void printMatrix(int A[][COLS])
+{
+	int i, j;
+	for(i=0;i<ROWS;i++)
+	{
+		for(j=0;j<COLS;j++)
+		 printf("%i ", A[i][j]);
+		printf("\n");
+	}
+}
+
 int main(int argc, char*argv[])
 {
-	int i;
-	int a[SIZE]={1, 2, 3, 4, 5};
-	int b[SIZE]={1, 2, 10, 4, 5};
-	int flag_same=1;
-	
-	for(i=0;i<SIZE;i++)
+	int A[ROWS][COLS]=
 	{
-		if(a[i]!=b[i])
-		{
-			printf("%i is different\n", i);
-		}
-	}
+		{2, 3, 0},
+		{8, 9, 1},
+		{7, 0, 5}
+	};
+	
+	int B[ROWS][COLS]=
+	{
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1}
+	};
+	
+	int C[ROWS][COLS];
+	
+	addMatrix(A, B, C);
+	printMatrix(C);
 	 
 	return 0;
 }
